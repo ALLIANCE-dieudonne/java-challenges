e8aec65d20e18d6400de13ba1d11c8bf21bf0807
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-09-28 10:10:19 +0200

    sum of two numbers

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,18 @@
+
+public class SumOfTwoNumbers {
+
+  public static int sum(int n1, int n2) {
+    return n1 + n2;
+  }
+
+  public static void main(String[] args) {
+
+    int result1 = sum(1,3);
+
+    System.out.println(result1);
+
+    int result2 = sum(2,4);
+    System.out.println(result2);
+
+  }
+}
