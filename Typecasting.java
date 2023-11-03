43824279f9ededcce318467ab9e34f718a0530f0
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-11-03 14:08:05 +0200

    typecasting

--- Typecasting.java
+++ Typecasting.java
@@ -1,4 +1,8 @@
 class Animal {
+
+  double d = 23.5;
+  int n = (int) d;
+
   void makeSound() {
     System.out.println("Some sound!");
   }
@@ -15,10 +19,10 @@ class Dog extends Animal {
 
 public class Typecasting {
   public static void main(String[] args) {
-    Animal dog = new Dog();//down casting
+    Animal dog = new Dog();//upcasting casting
     dog.makeSound();
 
-    Dog myActualDog = (Dog) dog;//upcasting
+    Dog myActualDog = (Dog) dog;//down-casting upcasting
     dog.makeSound();
   }
 }
