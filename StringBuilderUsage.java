b3532944ffddd0ca7e525b536010e2adf567692e
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-11-03 14:02:06 +0200

    reversing string using stringBuilder

---
+++ StringBuilderUsage.java
@@ -0,0 +1,8 @@
+public class StringBuilderUsage {
+
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("fils");
+    System.out.println(sb.reverse());
+  }
+}
