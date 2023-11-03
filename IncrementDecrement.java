f14ae4d3860be4709349f8528a66e77f24bb54cb
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-11-03 14:04:53 +0200

    increment decrement challange

--- IncrementDecrement.java
+++ IncrementDecrement.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class IncrementDecremen {
+public class IncrementDecrement {
+
+  public static void main(String[] args) {
+    int i = 0;
+    int j = i;
+    j = i++ +j;
+    System.out.println(i + "-" + j);
+
+    /*
+    options
+      0-0
+      1 - 0
+      0 - 1
+      1 - 1
+    */
+
+  }
 }
